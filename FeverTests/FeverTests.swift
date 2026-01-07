ab7c9b180538629f7ac881f40a4b1288052acfaf
Gavin <gholtzap@asu.edu>
2026-01-07 13:46:15 -0500

    Initial Commit

---
+++ FeverTests/FeverTests.swift
@@ -0,0 +1,17 @@
+//
+//  FeverTests.swift
+//  FeverTests
+//
+//  Created by Gavin Holtzapple on 1/7/26.
+//
+
+import Testing
+@testable import Fever
+
+struct FeverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ FeverUITests/FeverUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  FeverUITestsLaunchTests.swift
+//  FeverUITests
+//
+//  Created by Gavin Holtzapple on 1/7/26.
+//
+
+import XCTest
+
+final class FeverUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
